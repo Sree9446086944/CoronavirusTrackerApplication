482e41ca2d120d9b17917e8f22743019d273c7de
Sreekanth Kumar <sreekanthkumardbz@gmail.com>
2022-02-26 14:48:04 +0530

    added csv parser library-apache commons_ parsed fetched raw csv to csv object

--- src/main/java/com/covidTracker/service/CovidTrackerService.java
+++ src/main/java/com/covidTracker/service/CovidTrackerService.java
@@ -1,6 +1,7 @@
 package com.covidTracker.service;
 
 import java.io.IOException;
+import java.io.StringReader;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
@@ -8,6 +9,9 @@ import java.net.http.HttpResponse;
 
 import javax.annotation.PostConstruct;
 
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -15,7 +19,8 @@ public class CovidTrackerService {
 	
 	private static String COVID_DATA_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
 
-	@PostConstruct 
+	@PostConstruct
+	@Scheduled(cron="* * * * * *")  // run every second
 	public void fetchCovidData() throws IOException, InterruptedException {
 		HttpClient client =  HttpClient.newHttpClient();  //creating new client
 		HttpRequest request = HttpRequest.newBuilder()      // to use builder pattern
@@ -23,7 +28,14 @@ public class CovidTrackerService {
 		       .build();  // build the request
 		HttpResponse<String> httpResponse = client.send(request, HttpResponse.BodyHandlers.ofString());  // add exceptions since throws exception when client.send fails
 		//client.send() - first argument - request itself, second arg - what to do with response, here to string
-		System.out.println(httpResponse.body());
+//		System.out.println(httpResponse.body());
+		StringReader csvBodyReader = new StringReader(httpResponse.body()); // we have reader from string instance
+		Iterable<CSVRecord> records = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(csvBodyReader); //pass reader and parse to csv
+		for (CSVRecord record : records) {
+		    String state = record.get("Province/State");
+		    System.out.println(state);
+		}
+		     
 		
 	}
 }
